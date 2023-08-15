6cd7a0cff242fd4490e592d103ac9d55c2e39c3b
Anna Tasheva <anna.tasheva90@gmail.com>
2023-08-15 10:49:49 +0100

    fixed bugs in order to deply the app

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,17 +1,17 @@
-'use client'
+"use client";
 import React from "react";
-import { useAuthContext } from "@/context/AuthContext";
+import { useAuthContext } from "../context/AuthContext";
 import { useRouter } from "next/navigation";
-import {useContext} from 'react'
+import { useContext } from "react";
 
 function Page() {
-    const { user } = useContext(useAuthContext())
-    const router = useRouter()
+    const { user } = useContext(useAuthContext());
+    const router = useRouter();
     React.useEffect(() => {
-        if (user == null) router.push("/login")
-    }, [user])
+        if (user == null) router.push("/login");
+    }, [user]);
 
-    return (<h1>Only logged in users can view this page</h1>);
+    return <h1>Only logged in users can view this page</h1>;
 }
 
 export default Page;
